718aa7998145f26d52992fa204a54409cbf8d2e9
Adrian Anton Ladia <ladia.adrian@gmail.com>
2025-01-15 09:26:08 +0800

    Update app.py

--- Server/app.py
+++ Server/app.py
@@ -554,18 +554,17 @@ def removeRolefromUser():
 
 
 if __name__ == '__main__':
-    # if (AppConfig().getIsDevEnvironment()):
-    #     print(
-    #         f"\033[92m_______________________{AppConfig().getEnvironment().upper()}_______________________\033[0m"
-    #     )
-    # if AppConfig().getIsProductionEnvironment():
-    #     print(
-    #         f"\033[91m_______________________{AppConfig().getEnvironment().upper()}_______________________\033[0m"
-    #     )
+    if (AppConfig().getIsDevEnvironment()):
+        print(
+            f"\033[92m_______________________{AppConfig().getEnvironment().upper()}_______________________\033[0m"
+        )
+    if AppConfig().getIsProductionEnvironment():
+        print(
+            f"\033[91m_______________________{AppConfig().getEnvironment().upper()}_______________________\033[0m"
+        )
 
-    # if AppConfig().getisLocalEnvironment():
-    #     # dev
-    #     app.run(debug=False, host='0.0.0.0', port=5000)
-    # else:
-    # production
-    app.run(host='0.0.0.0', port=8080)
+    if AppConfig().getisLocalEnvironment():
+        # dev
+        app.run(debug=False, host='0.0.0.0', port=5000)
+    else:
+        app.run(host='0.0.0.0', port=8080)

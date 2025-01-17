88cbadb8ce9fdf39e83aceb48eda58778fa8eb0e
ValLetigio <valletigio54@gmail.com>
2025-01-17 11:35:30 +0800

    .

--- src/app/Offense/page.tsx
+++ src/app/Offense/page.tsx
@@ -46,7 +46,7 @@ const page = async () => {
         {/* action buttons */}
         <div className="w-full md:w-[80vw] 2xl:w-[70vw] flex flex-col md:flex-row justify-between bg-base-200 rounded-t-box px-2 border border-b-none">
           {/* <div className="flex justify-evenly md:justify-start items-center gap-4 px-2 py-2 rounded-box"> */}
-          <div className=" swap flex gap-3 items-center justify-start pl-2 max-w-full md:max-w-[50vw] overflow-x-auto">
+          <div className=" swap flex gap-3 items-center justify-start pl-2 ">
             <input 
               type="checkbox" 
               defaultChecked 

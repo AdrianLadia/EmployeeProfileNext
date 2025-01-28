997e86fafdea1c1f1342fedce55a95e2b6e57c84
FloresTristan <michaeltristanflores646@gmail.com>
2025-01-28 11:34:27 +0800

    updates

--- Server/app.py
+++ Server/app.py
@@ -132,7 +132,7 @@ def create_employee():
                     'dateJoined': data['dateJoined'],
                     'company': data['company'],
                     'isRegular': data['isRegular'],
-                    'isProductionEmployee': data['isProductionEmployee'],
+                    'companyRole': data['companyRole'],
                     'isOJT': data['isOJT'],
                     'dailyWage': data['dailyWage'],
                     '_version': 0
